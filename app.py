ba3bddc21d3ceaf0aefa6382e380aa3acc5165f7
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2025-02-09 03:15:29 -0500

    bollinger band class

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from historical.Overlap.BBhistorical import BollingerBands
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return "Hello, World!"
+@app.route('/bb', methods=['GET'])
+def bollinger_band():
+    model = BollingerBands()
+    return model.generate_chart()
 
 if __name__ == '__main__':
     app.run(port=5328)
--- historical/Overlap/BBhistorical.py
+++ historical/Overlap/BBhistorical.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 import talib
 import plotly.graph_objects as go
-import plotly.express as px
+import plotly.io as pio
 import pandas
 
 load_dotenv()
@@ -17,120 +17,134 @@ API_SECRET = os.getenv("API_SECRET")
 
 client = StockHistoricalDataClient(API_KEY,  API_SECRET)
 
+class BollingerBands:
 
-barsReq = StockBarsRequest(
-    symbol_or_symbols="MSFT",
-    timeframe=TimeFrame(1, TimeFrame.Minute),
-    start=datetime.datetime(2024, 2, 1, 10),
-    end=datetime.datetime(2024, 2, 2, 20),
-    limit=50
-)
+    def __init__(self, 
+                 symbol, 
+                 limit = 50,
+                 smaTime = 5,
+                 stdUp = 1,
+                 stdDown = 1) -> None:
 
-# Define request
-stockBars = client.get_stock_bars(barsReq)
+        self.symbol = symbol
+        self.limit = limit
+        self.smaTime = smaTime
+        self.stdUp = stdUp
+        self.stdDown = stdDown
 
-mid = []
-openVar = []
-highVar = []
-lowVar = []
-closeVar = []
-time = []
+    def generate_chart(self):
+        barsReq = StockBarsRequest(
+            symbol_or_symbols=self.symbol,
+            timeframe=TimeFrame(1, TimeFrame.Minute),
+            start=datetime.datetime(2024, 2, 1, 10),
+            end=datetime.datetime(2024, 2, 2, 20),
+            limit=self.limit
+        )
 
-for bar in stockBars:
-    data = bar[1]
-    for symbol_data in data.values():
-        for data_point in symbol_data:
-            middle = (data_point.high + data_point.low) / 2
-            mid.append(middle)
-            openVar.append(data_point.open)
-            highVar.append(data_point.high)
-            lowVar.append(data_point.low)
-            closeVar.append(data_point.close)
-            # DTM = data_point.timestamp
-            # time.append(int(DTM.minute) + int(DTM.hour) * (60))
-            time.append(data_point.timestamp)
+        # Define request
+        stockBars = client.get_stock_bars(barsReq)
 
+        mid = []
+        openVar = []
+        highVar = []
+        lowVar = []
+        closeVar = []
+        time = []
 
-candlestick_fig = go.Figure(data=[go.Candlestick(x=time,
-                open=openVar,
-                high=highVar,
-                low=lowVar,
-                close=closeVar)])
-
-arr = np.array(mid)
-
-smaTime = 5
-stdUp = 1
-stdDown = 1
-
-upperband, middleband, lowerband = talib.BBANDS(arr, timeperiod=smaTime, nbdevup=stdUp, nbdevdn=stdDown, matype=0)
-
-fig = go.Figure(data=candlestick_fig.data)
-
-fig.add_trace(go.Scatter(x=time, y=arr, mode="lines", name="Price", line=dict(color="blue")))
-
-fig.add_trace(go.Scatter(x=time, y=upperband, mode="lines", name="Upper Band", line=dict(color="red", dash="dot")))
-fig.add_trace(go.Scatter(x=time, y=middleband, mode="lines", name="Middle Band (SMA)", line=dict(color="black", dash="dash")))
-fig.add_trace(go.Scatter(x=time, y=lowerband, mode="lines", name="Lower Band", line=dict(color="green", dash="dot")))
-
-fig.update_layout(
-    title="Bollinger Bands Visualization",
-    xaxis_title="Time",
-    yaxis_title="Price",
-    legend=dict(x=0, y=1)
-)
+        for bar in stockBars:
+            data = bar[1]
+            for symbol_data in data.values():
+                for data_point in symbol_data:
+                    middle = (data_point.high + data_point.low) / 2
+                    mid.append(middle)
+                    openVar.append(data_point.open)
+                    highVar.append(data_point.high)
+                    lowVar.append(data_point.low)
+                    closeVar.append(data_point.close)
+                    # DTM = data_point.timestamp
+                    # time.append(int(DTM.minute) + int(DTM.hour) * (60))
+                    time.append(data_point.timestamp)
 
 
-cash = 100000
-stocks = 0
-amt = 100
-repeat = True
-buy = False
+        candlestick_fig = go.Figure(data=[go.Candlestick(x=time,
+                        open=openVar,
+                        high=highVar,
+                        low=lowVar,
+                        close=closeVar)])
 
-dfBuy = pandas.DataFrame({
-    "x": [],
-    "y": [],
-    "color": []  
-})
+        arr = np.array(mid)
 
-dfSell = pandas.DataFrame({
-    "x": [],
-    "y": [],
-    "color": []  
-})
+        smaTime = self.smaTime
+        stdUp = self.stdUp
+        stdDown = self.stdDown
 
-for i in range(len(mid)):
-    if(middleband[i] != np.nan and mid[i] <= lowerband[i] and (cash - amt * mid[i] > 10000) and (repeat or not buy)):
-        stocks += amt
-        cash -= amt * mid[i]
-        dfBuy.loc[len(dfBuy)] = [time[i], mid[i], "green"]
-        print(f"We bought {amt} stocks for {mid[i]} on {i} because the {lowerband[i]} value")
-        buy = True
-    elif(middleband[i] != np.nan and mid[i] >= upperband[i] and (stocks >= amt) and (repeat or buy)):
-        cash += amt * mid[i]
-        stocks -= amt
-        dfSell.loc[len(dfSell)] = [time[i], mid[i], "red"]
-        print(f"We sold {amt} stocks for {mid[i]} on {i} because the {upperband[i]} value")
+        upperband, middleband, lowerband = talib.BBANDS(arr, timeperiod=smaTime, nbdevup=stdUp, nbdevdn=stdDown, matype=0)
+
+        fig = go.Figure(data=candlestick_fig.data)
+
+        fig.add_trace(go.Scatter(x=time, y=arr, mode="lines", name="Price", line=dict(color="blue")))
+
+        fig.add_trace(go.Scatter(x=time, y=upperband, mode="lines", name="Upper Band", line=dict(color="red", dash="dot")))
+        fig.add_trace(go.Scatter(x=time, y=middleband, mode="lines", name="Middle Band (SMA)", line=dict(color="black", dash="dash")))
+        fig.add_trace(go.Scatter(x=time, y=lowerband, mode="lines", name="Lower Band", line=dict(color="green", dash="dot")))
+
+        fig.update_layout(
+            title="Bollinger Bands Visualization",
+            xaxis_title="Time",
+            yaxis_title="Price",
+            legend=dict(x=0, y=1)
+        )
+
+
+        cash = 100000
+        stocks = 0
+        amt = 100
+        repeat = True
         buy = False
 
-fig.add_scatter(
-    x=dfBuy["x"],
-    y=dfBuy["y"],
-    mode="markers",
-    marker=dict(size=10, color=dfBuy["color"]),  # Use the custom colors
-    name="Buy"
-)
+        dfBuy = pandas.DataFrame({
+            "x": [],
+            "y": [],
+            "color": []  
+        })
 
-fig.add_scatter(
-    x=dfSell["x"],
-    y=dfSell["y"],
-    mode="markers",
-    marker=dict(size=10, color=dfSell["color"]),  # Use the custom colors
-    name="Sell"
-)
+        dfSell = pandas.DataFrame({
+            "x": [],
+            "y": [],
+            "color": []  
+        })
 
-total = cash + stocks * mid[-1]
+        for i in range(len(mid)):
+            if(middleband[i] != np.nan and mid[i] <= lowerband[i] and (cash - amt * mid[i] > 10000) and (repeat or not buy)):
+                stocks += amt
+                cash -= amt * mid[i]
+                dfBuy.loc[len(dfBuy)] = [time[i], mid[i], "green"]
+                # print(f"We bought {amt} stocks for {mid[i]} on {i} because the {lowerband[i]} value")
+                buy = True
+            elif(middleband[i] != np.nan and mid[i] >= upperband[i] and (stocks >= amt) and (repeat or buy)):
+                cash += amt * mid[i]
+                stocks -= amt
+                dfSell.loc[len(dfSell)] = [time[i], mid[i], "red"]
+                # print(f"We sold {amt} stocks for {mid[i]} on {i} because the {upperband[i]} value")
+                buy = False
 
+        fig.add_scatter(
+            x=dfBuy["x"],
+            y=dfBuy["y"],
+            mode="markers",
+            marker=dict(size=10, color=dfBuy["color"]),  # Use the custom colors
+            name="Buy"
+        )
 
-print(f"started with {100000} got to {total} ended with {stocks} stocks and {cash} cash")
-fig.show()
+        fig.add_scatter(
+            x=dfSell["x"],
+            y=dfSell["y"],
+            mode="markers",
+            marker=dict(size=10, color=dfSell["color"]),  # Use the custom colors
+            name="Sell"
+        )
+
+        total = cash + stocks * mid[-1]
+
+        obj = pio.to_json(fig)
+        return obj
